624a9707af72a0409ee0c7b0bb4f8611c56ef964
Not-7Myself <98397618+Not-7Myself@users.noreply.github.com>
2024-08-01 15:07:32 +0530

    mom

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Event from "./Event";
 import Team from "./Team";
 import EventList from "./EventList";
 import Contact from "./Contact";
+import Construction from "./Construction.js";
 
 // src/App.js
 import React from "react";
@@ -26,18 +27,29 @@ function App() {
   return (
     <>
       <Router>
-        <NavBar />
+        ={" "}
         <div className="App">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/Events" element={<EventList />} />
+            <Route path="/" element={<Construction />} />
+            {/*<Route path="/Events" element={<EventList />} />
             <Route path="/Team" element={<Team />} />
-            <Route path="/Brochure" element={<Home />} />
-            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Contact" element={<Contact />} />*/}
             <Route
               path="/Register"
               element={
                 <ExternalRedirect to="https://forms.gle/CTaJthVc6Vz7hePs9" />
+              }
+            />
+            <Route
+              path="/Brochure"
+              element={
+                <ExternalRedirect to="https://drive.google.com/file/d/1j71EEUz4aIcSUUFUfbK-FdbUJ39z7kuB/view" />
+              }
+            />
+            <Route
+              path="/Teaser"
+              element={
+                <ExternalRedirect to="https://drive.google.com/file/d/1fSFA3EV-JXezE7LG5N8XXjb11S2tKzXM/view  " />
               }
             />
           </Routes>
